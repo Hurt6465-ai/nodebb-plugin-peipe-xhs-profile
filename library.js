df9efb21346336c418065ccbb2f39cfc8e871674
Hurt6465-ai <hurt6465@gmail.com>
2026-05-05 07:14:31 +0630

    Create library.js

---
+++ library.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Peipe XHS Mobile Profile
+ * Front-end-only NodeBB plugin.
+ *
+ * NodeBB loads this library when the plugin is activated. The actual UI work is
+ * done by public/lib/client.js and public/scss/xhs-profile.scss.
+ */
+module.exports = {};
